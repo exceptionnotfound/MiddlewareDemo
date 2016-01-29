38477b884ad43bbf12ecf0102a77b21995dc64d4
exceptionnotfound <mpjones428@gmail.com>
2016-01-29 07:50:35 -0700

    Added RequestHeaderMiddleware, skeleton for AuthorizationMiddleware, and MiddlewareExtensions.

---
+++ src/MiddlewareDemo/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareDemo.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestHeaderMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestHeaderMiddleware>();
+        }
+    }
+}
---
+++ src/MiddlewareDemo/Middleware/RequestHeaderMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareDemo.Middleware
+{
+    public class RequestHeaderMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestHeaderMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Headers.Keys.Contains("X-Cancel-Request"))
+            {
+                context.Response.StatusCode = 500;
+                return;
+            }
+
+            await _next.Invoke(context);
+
+            if (context.Request.Headers.Keys.Contains("X-Transfer-By"))
+            {
+                context.Response.Headers.Add("X-Transfer-Success", "true");
+            }
+        }
+    }
+}
