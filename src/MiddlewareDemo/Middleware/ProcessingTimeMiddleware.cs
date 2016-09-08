734116020fe059a8b9d795d464325e377e47cdd6
exceptionnotfound <mpjones428@gmail.com>
2016-09-08 06:53:59 -0700

    Formatting fix, added ProcessingTimeMiddleware

---
+++ src/MiddlewareDemo/Middleware/ProcessingTimeMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareDemo.Middleware
+{
+    public class ProcessingTimeMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ProcessingTimeMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            await _next(context);
+
+            context.Response.Headers.Add("X-Processing-Time-Milliseconds", new[] { watch.ElapsedMilliseconds.ToString() });
+        }
+    }
+}
